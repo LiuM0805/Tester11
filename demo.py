ccf56d6912407e57a4e72d1f001c3528a0808485
LiuM0805 <719998141@qq.com>
2020-01-28 10:02:24 +0800

    init

---
+++ demo.py
@@ -0,0 +1,14 @@
+print(2 + 2)
+
+print('c:\\user\\liumiao')
+print(r'c:\user\liumiao')
+
+print('Py' 'thon')
+
+x='abc'
+print(f'c:\\user\\liumiao\\{x}')
+print("c:\\user\\liumiao\\{y}\\{x}".format(y=x,x=123))
+
+squares = [1, 4, 9, 16, 25]
+print(squares[1:3])
+
