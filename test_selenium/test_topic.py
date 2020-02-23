5a69792f2b0cb04fb6d707e3950d1dbecd9ce8c4
LiuM0805 <719998141@qq.com>
2020-02-23 20:41:35 +0800

    1.录制代码导入：test_search.py 2.编写case：test_topic.py 3.元素定位 4.隐式等待&显式等待&强制等待

---
+++ test_selenium/test_topic.py
@@ -0,0 +1,26 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class Test_Testerhome:
+    def setup_method(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://testerhome.com/")
+        self.driver.implicitly_wait(5)
+
+    def test_testerhome(self):
+        self.driver.find_element(By.LINK_TEXT, '社团').click()
+        # sleep(2)
+        element = (By.XPATH,"//*[@data-name='霍格沃兹测试学院']")
+        WebDriverWait(self.driver, 20).until(expected_conditions.element_to_be_clickable(element))
+        self.driver.find_element(*element).click()
+        # self.driver.find_element(By.CSS_SELECTOR, '[data-name=霍格沃兹测试学院]').click()
+        # self.driver.find_element(By.CSS_SELECTOR, '.topic-22235 .title > a').click()
+        self.driver.find_element(By.CSS_SELECTOR, '.topic:nth-child(1) .title a').click()
+
+    def teardown(self):
+        sleep(1)
+        self.driver.quit()
