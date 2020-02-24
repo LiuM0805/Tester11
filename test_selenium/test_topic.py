e83e9b57f44387e0b4916aeb7aae96fa54807a53
LiuM0805 <719998141@qq.com>
2020-02-24 15:53:15 +0800

    1.封装了wait方法的方式传参

--- test_selenium/test_topic.py
+++ test_selenium/test_topic.py
@@ -11,11 +11,15 @@ class Test_Testerhome:
         self.driver.get("https://testerhome.com/")
         self.driver.implicitly_wait(5)
 
+    def wait(self, timeout, method):
+        WebDriverWait(self.driver, timeout).until(method)
+
     def test_testerhome(self):
         self.driver.find_element(By.LINK_TEXT, '社团').click()
         # sleep(2)
-        element = (By.XPATH,"//*[@data-name='霍格沃兹测试学院']")
-        WebDriverWait(self.driver, 20).until(expected_conditions.element_to_be_clickable(element))
+        # element = (By.CSS_SELECTOR, "[data-name='霍格沃兹测试学院']")
+        element=(By.PARTIAL_LINK_TEXT, '霍格沃兹测试学院')
+        self.wait(10, expected_conditions.element_to_be_clickable(element))
         self.driver.find_element(*element).click()
         # self.driver.find_element(By.CSS_SELECTOR, '[data-name=霍格沃兹测试学院]').click()
         # self.driver.find_element(By.CSS_SELECTOR, '.topic-22235 .title > a').click()
