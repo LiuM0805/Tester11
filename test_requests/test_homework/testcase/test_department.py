94cdd8c29d8b101ad093ade7cb67c7cdf6fb481e
LiuM0805 <719998141@qq.com>
2020-05-17 15:02:13 +0800

    打印每次接口访问结果

--- test_requests/test_homework/testcase/test_department.py
+++ test_requests/test_homework/testcase/test_department.py
@@ -18,14 +18,17 @@ class TestDepartment:
     # 测试创建部门
     def test_depart_create(self):
         r = self.depart.create_depart("家庭作业")
+        print(self.test_depart_select())
         assert r["errcode"] == 0
 
     # 测试更新部门
     def test_depart_update(self):
         r = self.depart.update_depart(id=3, name="家庭作业1")
+        print(self.test_depart_select())
         assert r["errcode"] == 0
 
     # 测试删除部门
     def test_depart_delete(self):
         r = self.depart.delete_depart(id=3)
+        print(self.test_depart_select())
         assert r["errcode"] == 0
